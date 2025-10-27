2368f936c03a82ad54f50511ef6afbed03999dc8
Jay Kadlag <jaykadlag14@gmail.com>
2025-10-27 11:20:01 +0530

    ci: add lint workflow to validate PR commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,5 @@
 const config = {
   extends: ["@commitlint/config-conventional"],
-  // rules: {
-  //   "type-enum": [
-  //     2,
-  //     "always",
-  //     [
-  //       "ci",
-  //       "chore",
-  //       "docs",
-  //       "feat",
-  //       "fix",
-  //       "perf",
-  //       "refactor",
-  //       "revert",
-  //       "style",
-  //       "assets",
-  //     ],
-  //   ],
-  // },
 };
 
 export default config;
