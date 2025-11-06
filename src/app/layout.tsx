2849febfb2add2465d969aafb686cb1da8134bce
Jay Kadlag <jaykadlag14@gmail.com>
2025-11-06 17:12:03 +0530

    feat: add toast for showing oauth signin errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const dmSans = DM_Sans({
 export const metadata: Metadata = {
   title: "mentora",
   description:
-    "A modern mental wellness guide inspired by timeless Bhagavad Gita teachings.",
+    "A modern AI-based mental wellness guide inspired by timeless Bhagavad Gita teachings.",
 };
 
 export default function GlobalLayout({children}: {children: React.ReactNode}) {
