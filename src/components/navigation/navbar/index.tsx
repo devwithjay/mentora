3d3bca15ec32d9a00943b34d9ace395480052244
Jay Kadlag <jaykadlag14@gmail.com>
2025-11-08 18:09:50 +0530

    feat: enhance navbar

--- src/components/navigation/navbar/index.tsx
+++ src/components/navigation/navbar/index.tsx
@@ -1,32 +1,36 @@
-"use client";
+import Link from "next/link";
 
-import {useState} from "react";
+import {LogOut, User} from "lucide-react";
+import {signOut} from "next-auth/react";
 
-import {Menu, X} from "lucide-react";
-
+import {getCurrentUser} from "@/auth";
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 import {Button} from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import ROUTES from "@/constants/routes";
 
 import Logo from "./logo";
 import Login from "./sign-in";
 import SignUp from "./sign-up";
 import Theme from "./theme";
 
-const Navbar = () => {
-  const isUser = false;
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
-  const toggleMobileMenu = () => {
-    setMobileMenuOpen(!mobileMenuOpen);
-  };
+const Navbar = async () => {
+  const user = await getCurrentUser();
 
   return (
-    <nav className="bg-primary flex w-full items-center justify-between gap-5 px-6 py-4 sm:px-12">
+    <nav className="flex w-full items-center justify-between gap-5 px-6 py-4 sm:px-12">
       <Logo showText />
 
-      <div className="flex gap-x-4">
+      <div className="flex gap-x-2 sm:gap-x-5">
         <Theme />
-        {!isUser ? (
+        {!user ? (
           <>
             <div className="flex gap-x-4 max-sm:hidden">
               <Login />
@@ -34,42 +38,45 @@ const Navbar = () => {
             </div>
           </>
         ) : (
-          <Avatar className="size-9 rounded-md">
-            <AvatarImage src="" alt="profile-img" />
-            <AvatarFallback className="rounded-md">
-              A<sup>2</sup>
-            </AvatarFallback>
-          </Avatar>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar className="hidden scale-110 cursor-pointer rounded-md sm:block">
+                <AvatarImage src={user.image || ""} alt={user.name!} />
+                <AvatarFallback className="rounded-md dark:bg-(--background-secondary)">
+                  {user.name?.charAt(0)}
+                </AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="bg-primary mt-2 mr-5 sm:mr-11">
+              <DropdownMenuLabel className="text-secondary text-sm font-light">
+                {user.username}
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="cursor-pointer focus:bg-(--background-secondary)">
+                <Link
+                  href={ROUTES.PROFILE(user.username)}
+                  className="flex items-center gap-2.5"
+                >
+                  <User className="h-4 w-4" />
+                  Profile
+                </Link>
+              </DropdownMenuItem>
+
+              <DropdownMenuItem className="cursor-pointer focus:bg-(--background-secondary)">
+                <Button
+                  type="submit"
+                  className="-ml-2.5 h-fit p-0 font-normal"
+                  variant="ghost"
+                  onClick={() => signOut()}
+                >
+                  <LogOut className="h-4 w-4" />
+                  Logout
+                </Button>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         )}
-
-        <Button
-          variant="ghost"
-          size="icon"
-          className="text-primary border-primary cursor-pointer border hover:bg-transparent sm:hidden"
-          onClick={toggleMobileMenu}
-        >
-          {mobileMenuOpen ? (
-            <X className="size-5" />
-          ) : (
-            <Menu className="size-5" />
-          )}
-        </Button>
       </div>
-
-      {mobileMenuOpen && (
-        <div
-          className={`bg-primary fixed inset-0 top-16 z-50 flex flex-col items-end px-6 py-4 transition-all duration-300 ease-in-out sm:hidden ${
-            mobileMenuOpen
-              ? "translate-y-0 opacity-100"
-              : "-translate-y-5 opacity-0"
-          }`}
-        >
-          <div className="flex flex-col items-end gap-3 pt-4">
-            <Login />
-            <SignUp />
-          </div>
-        </div>
-      )}
     </nav>
   );
 };
