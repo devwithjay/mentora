4d8bf9a8dede0739105ce52b618360596013c8a6
Jay Kadlag <jaykadlag14@gmail.com>
2025-11-09 18:30:02 +0530

    feat: add middleware to redirect authenticated users from auth routes

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,1 +1,22 @@
-export {auth as proxy} from "@/auth";
+import {Route} from "next";
+
+import {auth} from "@/auth";
+import ROUTES from "@/constants/routes";
+
+const middleware = auth(req => {
+  const {nextUrl} = req;
+  const isLoggedIn = Boolean(req.auth);
+
+  const isAuthRoute = [
+    ROUTES.SIGN_IN,
+    ROUTES.SIGN_UP,
+    ROUTES.AUTH_ERROR,
+  ].includes(nextUrl.pathname as Route);
+
+  if (isLoggedIn && isAuthRoute) {
+    const url = new URL(ROUTES.HOME, nextUrl.origin);
+    return Response.redirect(url);
+  }
+});
+
+export {middleware};
